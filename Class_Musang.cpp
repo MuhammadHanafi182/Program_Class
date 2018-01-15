f51bbf86f57cfed125de27744d6d1124d565f603
MuhammadHanafi182 <32392643+MuhammadHanafi182@users.noreply.github.com>
2018-01-15 21:33:51 +0700

    memasukan pembentuk output yang diberikan

--- Class_Musang.cpp
+++ Class_Musang.cpp
@@ -20,7 +20,18 @@ public:
     // Pembentuk atau outputan yang diberikan
 
     JenisMusang (string namamusang, string habitat, int panjangtubuhmaks, int brtmaks, 
-	string makanan, string warna, string karakter )    
+	string makanan, string warna, string karakter )   
+	
+     {
+    	JenisMusang::namamusang = namamusang;
+        JenisMusang::habitat = habitat;
+        JenisMusang::panjangtubuhmaks = panjangtubuhmaks;
+        JenisMusang::brtmaks = brtmaks;
+        JenisMusang::makanan = makanan;
+        JenisMusang::warna = warna;
+    	JenisMusang::karakter = karakter;   
+        
+     }
     
     
 int main(){
