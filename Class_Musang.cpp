c011dfc30ef3892f7a0aaf9ffbe31785230737bd
MuhammadHanafi182 <32392643+MuhammadHanafi182@users.noreply.github.com>
2018-01-15 21:24:18 +0700

    dan disini ulun membuat kategori public untuk pembentuk Output

--- Class_Musang.cpp
+++ Class_Musang.cpp
@@ -15,6 +15,12 @@ private:
     string warna;
     string karakter;
     
+public:
+
+    // Pembentuk atau outputan yang diberikan
+
+    JenisMusang (string namamusang, string habitat, int panjangtubuhmaks, int brtmaks, 
+	string makanan, string warna, string karakter )    
     
     
 int main(){
