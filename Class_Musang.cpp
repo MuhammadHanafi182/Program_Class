ee8f11d7eb99dbd7b899f53f05ab2849458310d9
MuhammadHanafi182 <32392643+MuhammadHanafi182@users.noreply.github.com>
2018-01-15 21:19:11 +0700

    Ulun memasukan Kategori Private Untuk Tipe data yang digunakan di dalam Output-output yang digunakan

--- Class_Musang.cpp
+++ Class_Musang.cpp
@@ -2,5 +2,20 @@
 
 using namespace std;
 
+class JenisMusang
+
+{
+private:
+
+	string namamusang;
+    string habitat;
+    int panjangtubuhmaks;
+    int brtmaks;
+    string makanan;
+    string warna;
+    string karakter;
+    
+    
+    
 int main(){
 }
